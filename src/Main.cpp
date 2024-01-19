476cd77fb5dc053df20555b1ff603383ce6f76c8
SebastianBirk-AI <84044936+SebastianBirk-AI@users.noreply.github.com>
2024-01-19 17:14:46 +0100

    Tick + rules

--- src/Main.cpp
+++ src/Main.cpp
@@ -17,17 +17,18 @@ const unsigned int SCR_WIDTH = 1000;
 const unsigned int SCR_HEIGHT = 1000;
 
 //creates global variables
-const unsigned int amount = 3 + 2;
+const unsigned int amount = 9 + 2;
 static GLfloat* testing = new GLfloat[(amount-2)*(amount-2) * 3];
-static float size = 0.1f;
+float size = 0.1f;
+
 
 //function declaration
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-bool CheckNeighbors(bool (*temp)[amount][amount], int x, int y);
+bool CheckNeighbors(int x, int y);
 
 //creates cell array
-bool cells[amount][amount] = {true};
-bool tempCells[amount][amount] = {false};
+bool cells[amount][amount];
+bool tempCells[amount][amount];
 
 const char *fragmentShaderSource= "#version 330 core\n"
     "out vec4 FragColor;\n"
@@ -49,7 +50,11 @@ int main()
 {
     //variable declaration
     float offset = 0.5f;
-    int neighborCounter = 0;
+    float currentTime = 0;
+    float lastTime = 0;
+    int index = 0;
+    bool timeActive = false;
+    bool moveForwardManual = false;
 
     //initialise GLFW
     glfwInit();
@@ -79,6 +84,7 @@ int main()
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
+    
     // check for shader compile errors
     int success;
     char infoLog[512];
@@ -152,42 +158,61 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, offsetBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * pow(amount,2), NULL, GL_STATIC_DRAW);
 
+    //For testing
     for (int i = 1; i < amount-1; i++)
     {
         for (int j = 1; j < amount-1; j++)
         {
-            if(j%2==0) cells[i][j] = true;
-            else cells[i][j] = false;
+            cells[i][j] = true;
         }
     }
     
 
     while (!glfwWindowShouldClose(window))
     {
-        //variable declaration
-        int index = 0;
-
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //updates the array and draws the live cells to the screen
-        for (int y = 1; y < amount-1; y++)
-        {
-            for (int x = 1; x < amount-1; x++)
-            {
-                //checks if a cell is alive
-                if (cells[y][x])
-                {
-                    //draws it to the graphics buffer
-                testing[3*index] = (float)x + offset;
-                testing[3*index+1] = (float)y + offset;
-                testing[3*index+2] = size;
-                index++;
-                }
+        //Tick controller
+        currentTime += glfwGetTime() - lastTime; 
+        lastTime = glfwGetTime();
 
-                //cell rules execution
-                //*pTempCell[x][y] = CheckNeighbors(pCell,x,y);
+        //Waits till 5 seconds have passed
+        std::cout << currentTime << std::endl;
+        if ((currentTime > 5.0f && timeActive) || moveForwardManual)
+        {
+            //resets moveForwardManual
+            moveForwardManual = false;
+
+            //resets current time
+            currentTime = 0;
+
+            //resets index
+            index = 0;
+
+            //updates the array and draws the live cells to the screen
+            for (int y = 1; y < amount-1; y++)
+            {
+                for (int x = 1; x < amount-1; x++)
+                {
+                    //updates the temp array based on Conways rules
+                    tempCells[y][x] = CheckNeighbors(x,y);
+
+                    //checks if a cell is alive
+                    if (!cells[y][x]) continue;
+                
+                    //draws it to the graphics buffer
+                    testing[3*index] = (float)(x-1) + offset;
+                    testing[3*index+1] = (float)(y-1) + offset;
+                    testing[3*index+2] = size;
+                    
+                    //counts index up
+                    index++;
+                }
             }
+
+            //Updates the cell array
+            std::copy(&tempCells[0][0], &tempCells[0][0]+amount*amount, &cells[0][0]);
         }
 
         //buffer the data
@@ -231,8 +256,28 @@ bool CheckNeighbors(int x, int y)
     //checks around the cell
     for (int i = 1; i < 9; i++)
     {
-        if(cells[x-(int)round(cos((6.28/8)*i))][y-(int)round(sin((6.28/8)*i))]) counter++;
+        if(cells[y-(int)round(cos((6.28/8)*i))][x-(int)round(sin((6.28/8)*i))]) counter++;
     }
 
+    //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    if (!cells[y][x] && counter == 3)
+    {
+        return true;
+    }
+    
+    //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    //Any live cell with more than three live neighbours dies, as if by overpopulation.
+    if (cells[y][x] && (counter<2 || counter > 3))
+    {
+        return false;
+    }
+
+    //Any live cell with two or three live neighbours lives on to the next generation. 
+    if (cells[y][x] && (counter == 2 || counter == 3))
+    {
+        return true;
+    }
+
+    //default for exceptions
     return false;
 }
